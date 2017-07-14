134b17353839229a923bfc8fff4b9d16b2edfab7
B3nac <Rockapparatus@yahoo.com>
2017-07-14 14:16:58 -0700

    Making it better.

--- auto_redirect.py
+++ auto_redirect.py
@@ -1,7 +1,12 @@
 import requests
 
+url = 'http://vudu.com'
 payload = '//google.com'
 
-r = requests.get('https://vudu.com/', payload)
-print(r.url)
+r = requests.get(url, payload, timeout=1)
+
+print(r.url,"\n")
+
+print(r.headers, "\n")
+print(r.status_code, "\n")
 
